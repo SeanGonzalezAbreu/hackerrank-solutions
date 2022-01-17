c1329978654f355ea83b3b9cb6d4659876b1cc7f
Sean González <sgonzalez@teravisiontech.com>
2022-01-17 12:11:36 -0400

    solved 4th hashmap challenge: count triplets.

--- solutions/dictionaries-and-hashmaps/count-triplets/count-triplets.ts
+++ solutions/dictionaries-and-hashmaps/count-triplets/count-triplets.ts
@@ -4,8 +4,30 @@ const TEST_NUMBERS = [1, 3, 9, 9, 27, 81]
 const COMMON_RATIO = 3;
 
 export const countTriplets = (numbers: number[], ratio: number) => {
-    // WIP
-    return 0;
+    const secondHashMap: { [key: number]: number } = {},
+        thirdHashMap: { [key: number]: number } = {};
+
+    let numberOfTriplets = 0;
+
+    for (const number of numbers) {
+        if (thirdHashMap[number]) {
+            numberOfTriplets += thirdHashMap[number];
+        }
+
+        if (secondHashMap[number]) {
+            thirdHashMap[number * ratio] =
+                thirdHashMap[number * ratio]
+                    ? thirdHashMap[number * ratio] + secondHashMap[number]
+                    : secondHashMap[number];
+        }
+
+        secondHashMap[number * ratio] =
+            secondHashMap[number * ratio]
+                ? secondHashMap[number * ratio] + 1
+                : 1;
+    }
+
+    return numberOfTriplets;
 }
 
 countTriplets(TEST_NUMBERS, COMMON_RATIO); // 6
